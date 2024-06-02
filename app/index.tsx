0ce32c4db85e7492b63cf515f1749e2a4be1d6c4
Tarasov-Dima <dmytrotarasov9@gmail.com>
2024-06-02 13:07:33 +0200

    feat(app): initial commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import firebase from "../FirebaseConfig";
+
+export default function Index() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				// justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Text style={{ fontSize: 40 }}>Lena жопа</Text>
+		</View>
+	);
+}
