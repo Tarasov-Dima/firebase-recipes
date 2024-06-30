0522f154b1266e7288d432230d9f79442f21fb90
Tarasov-Dima <dmytrotarasov9@gmail.com>
2024-06-30 18:04:44 +0200

    fix(github-actions): typo

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
-	authDomain: process.env.FIREBASE_PUBLIC_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PUBLIC_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_PUBLIC_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_PUBLIC_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_PUBLIC_APP_ID,
-	measurementId: process.env.FIREBASE_PUBLIC_MEASUREMENT_ID,
+	apiKey: process.env.EXPO_PUBLIC_API_KEY,
+	authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+	projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+	storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+	messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+	appId: process.env.EXPO_PUBLIC_APP_ID,
+	measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
