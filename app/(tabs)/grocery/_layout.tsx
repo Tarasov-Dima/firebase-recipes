295393a30c90264e8f1c1638585ce7bf73d5e6be
Tarasov-Dima <dmytrotarasov9@gmail.com>
2024-10-09 18:31:44 +0200

    feat(theme): added theme to other main headers

--- app/(tabs)/grocery/_layout.tsx
+++ app/(tabs)/grocery/_layout.tsx
@@ -1,12 +1,24 @@
-import { useGroceryList } from "@/storage/useGroceryList";
+import { useThemeContext } from "@/theme";
 import { Stack } from "expo-router";
+import { IconButton } from "react-native-paper";
 
 const StackLayout = () => {
+	const { toggleTheme, isDarkTheme } = useThemeContext();
+
 	return (
 		<Stack>
 			<Stack.Screen
 				name='index'
-				options={{ headerShown: true, headerTitle: "Grocery" }}
+				options={{
+					headerShown: true,
+					headerTitle: "Grocery",
+					headerRight: () => (
+						<IconButton
+							icon={isDarkTheme ? "brightness-7" : "brightness-3"}
+							onPress={toggleTheme}
+						/>
+					),
+				}}
 			/>
 		</Stack>
 	);
--- app/(tabs)/settings/_layout.tsx
+++ app/(tabs)/settings/_layout.tsx
@@ -1,11 +1,24 @@
+import { useThemeContext } from "@/theme";
 import { Stack } from "expo-router";
+import { IconButton } from "react-native-paper";
 
 const StackLayout = () => {
+	const { toggleTheme, isDarkTheme } = useThemeContext();
+
 	return (
 		<Stack>
 			<Stack.Screen
 				name='index'
-				options={{ headerShown: true, headerTitle: "Settings" }}
+				options={{
+					headerShown: true,
+					headerTitle: "Settings",
+					headerRight: () => (
+						<IconButton
+							icon={isDarkTheme ? "brightness-7" : "brightness-3"}
+							onPress={toggleTheme}
+						/>
+					),
+				}}
 			/>
 			<Stack.Screen
 				name='user'
